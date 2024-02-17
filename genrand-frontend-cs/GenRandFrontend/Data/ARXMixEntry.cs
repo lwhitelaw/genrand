988377e32a0a341e594ca1ca6a7d6302d1c3939d
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2024-02-17 16:59:36 -0500

    Work on API

---
+++ genrand-frontend-cs/GenRandFrontend/Data/ARXMixEntry.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+
+namespace GenRandFrontend.Data
+{
+	/// <summary>
+	/// Contains info about an ARX-based mix function that the API will return. Based on the API-side class of the same name.
+	/// The fields have identical meanings.
+	/// </summary>
+	public class ARXMixEntry
+	{
+		public string Type { get; }
+		public long Definition { get; }
+		public double AvScore1 { get; }
+		public double AvScore2 { get; }
+		public double AvScore3 { get; }
+		public double AvScore4 { get; }
+		public string AvImage1 { get; }
+		public string AvImage2 { get; }
+		public string AvImage3 { get; }
+		public string AvImage4 { get; }
+
+		[JsonConstructor]
+		public ARXMixEntry(string type, long definition, double avScore1, double avScore2, double avScore3, double avScore4, string avImage1, string avImage2, string avImage3, string avImage4)
+		{
+			Type = type;
+			Definition = definition;
+			AvScore1 = avScore1;
+			AvScore2 = avScore2;
+			AvScore3 = avScore3;
+			AvScore4 = avScore4;
+			AvImage1 = avImage1;
+			AvImage2 = avImage2;
+			AvImage3 = avImage3;
+			AvImage4 = avImage4;
+		}
+
+		public override string ToString()
+		{
+			return $"{Type},{Definition},{AvScore1},{AvScore2},{AvScore3},{AvScore4},{AvImage1},{AvImage2},{AvImage3},{AvImage4}";
+		}
+	}
+}
