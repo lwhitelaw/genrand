8a352340e33a16c33ea1879b4635cdacf5e73276
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2024-02-17 20:35:01 -0500

    Work on decoding API objects for display

--- genrand-frontend-cs/GenRandFrontend/Data/ARXMixEntry.cs
+++ genrand-frontend-cs/GenRandFrontend/Data/ARXMixEntry.cs
@@ -39,5 +39,82 @@ namespace GenRandFrontend.Data
 		{
 			return $"{Type},{Definition},{AvScore1},{AvScore2},{AvScore3},{AvScore4},{AvImage1},{AvImage2},{AvImage3},{AvImage4}";
 		}
+
+		private static readonly string[] TYPES = new string[]
+		{
+			"8x2", "8x3", "8x4",
+			"16x2", "16x3", "16x4",
+			"32x2", "32x3", "32x4",
+			"64x2", "64x3", "64x4"
+		};
+		private static readonly int[] ROT_BITS = new int[] { 
+			3, 3, 3, // 8x2, 8x3, 8x4
+			4, 4, 4, // 16x2, 16x3, 16x4
+			5, 5, 5, // 32x2, 32x3, 32x4
+			6, 6, 6 // 64x2, 64x3, 64x4
+		};
+		private static readonly int[] TERMS = { 
+			4, 6, 8, // 8x2, 8x3, 8x4
+			4, 6, 8, // 16x2, 16x3, 16x4
+			4, 6, 8, // 32x2, 32x3, 32x4
+			4, 6, 8 // 64x2, 64x3, 64x4
+		};
+
+		private static int LookupInfoByTypeString(string type)
+		{
+			for (int i = 0; i < TYPES.Length; i++) {
+				if (TYPES[i].Equals(type)) { return i; }
+			}
+			return -1;
+		}
+		public void DecodeDefinition(string type, long definition, out int[] rotations, out bool[] xors)
+		{
+			ulong def = (ulong)definition;
+			int typeIndex = LookupInfoByTypeString(type);
+			if (typeIndex == -1)
+			{
+				throw new ArgumentException("Type is not known");
+			}
+			int terms = TERMS[typeIndex];
+			int bits = ROT_BITS[typeIndex];
+			rotations = new int[terms];
+			xors = new bool[terms];
+			// unpack value - note indices are in reverse order
+			// write order wrote in xor bools followed by rotations a,b,c,d, etc. in that order
+			// therefore must unpack in reverse
+
+			// unpack rotations
+			for (int i = terms-1; i >= 0; i--)
+			{
+				// mask off needed bits and cast down to write in
+				rotations[i] = (int)(def & ((1U << bits) - 1));
+				// shift definition bits over
+				def = (def >> bits);
+			}
+
+			// unpack xors
+			for (int i = terms - 1; i >= 0; i--)
+			{
+				// mask off needed bits and cast down to write in
+				xors[i] = (def & 0x1) == 0x1;
+				// shift definition bits over
+				def = (def >> 1);
+			}
+		}
+
+		public string GetTerseOperationString()
+		{
+			StringBuilder sb = new StringBuilder();
+			int[] rotations;
+			bool[] xors;
+			DecodeDefinition(Type, Definition, out rotations, out xors);
+			for (int i = 0; i < rotations.Length; i++)
+			{
+				sb.Append(xors[i] ? "XOR " : "ADD ");
+				sb.Append("ROTL(").Append(rotations[i]).Append(")");
+				if (i != rotations.Length-1) sb.Append(" ");
+			}
+			return sb.ToString();
+		}
 	}
 }
