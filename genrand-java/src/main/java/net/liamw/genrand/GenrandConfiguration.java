8d68c003733e91e3ac809c2aa4ae3c9411c68001
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2024-02-21 14:42:52 -0500

    fix Docker oversight where genrand.db cannot have journals

--- genrand-java/src/main/java/net/liamw/genrand/GenrandConfiguration.java
+++ genrand-java/src/main/java/net/liamw/genrand/GenrandConfiguration.java
@@ -1,5 +1,9 @@
 package net.liamw.genrand;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -20,10 +24,15 @@ public class GenrandConfiguration {
 	 */
 	@Bean
 	public DataSource getDataSource() {
+		try {
+			Files.createDirectories(Paths.get("./data"));
+		} catch (IOException e) {
+			throw new RuntimeException("could not create needed directory");
+		}
 //		DataSource source = new DriverDataSource("jdbc:sqlite:./genrand-test.db", "org.sqlite.JDBC", properties, null, null);
 		HikariConfig config = new HikariConfig();
 		config.setDriverClassName("org.sqlite.JDBC");
-		config.setJdbcUrl("jdbc:sqlite:./genrand.db");
+		config.setJdbcUrl("jdbc:sqlite:./data/genrand.db");
 		config.setMaximumPoolSize(1);
 		config.setConnectionTimeout(Long.MAX_VALUE); // wait forever to get a connection if needed
 		DataSource source = new HikariDataSource(config);
